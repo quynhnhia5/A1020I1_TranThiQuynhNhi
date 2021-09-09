a40c6fc4d0aab92ea7cf1be1b32bba6f5b666d9d
quynhnhia5 <quynhnhia5@gmail.com>
2021-09-09 09:41:42 +0700

    Update

--- Module03/1.Relational database/Exercise/Thực thi các câu lệnh SQL/test.sql
+++ Module03/1.Relational database/Exercise/Thực thi các câu lệnh SQL/test.sql
@@ -8,6 +8,3 @@ SELECT * FROM customers WHERE customerName = 'Atelier Graphique';
 SELECT * FROM customers WHERE customername like '%A%';
 
 SELECT * FROM customers WHERE city IN ('Nantes',' Las Vegas',' Warszawa','NYC') ;
-
-
-
